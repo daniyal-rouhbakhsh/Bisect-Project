8e657e6befe1ef5eaf5b581dab90e7be626394e6
daniyal <daniyalrouhbakhsh@gmail.com>
2024-11-01 12:22:21 +0330

    isLogin Feature

--- JS/app.js
+++ JS/app.js
@@ -4,3 +4,12 @@ const mainForm = document.getElementById('main-form')
 formBtn.addEventListener('click',e => {
     e.preventDefault()
 })
+
+const isLogin = user => {
+    if(user){
+        return true
+    }
+    else{
+        return false
+    }
+}
